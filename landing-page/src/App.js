775a11a9628ace27fda3785b88744086c461d147
Noor ul ain Ibrahim <noorulainibrahim75@gmail.com>
2022-09-08 10:29:30 +0400

    react landing page

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { AboutUs, Cart, Feature, Footer, Header, Menu } from './container'
-import { Brand, Cta, Features, Food, Navbar } from './components'
+import { AboutUs, Cart, Feature, Footer, Header, Menu } from './container';
+import { Brand, Cta, Features, Food, Navbar } from './components';
+import './App.css';
 
 const App = () => {
   return (
--- landing-page/src/components/navbar/Navbar.jsx
+++ landing-page/src/components/navbar/Navbar.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import logo from '../../assets/logo.png'
+
+
+// Block Element Modifier
 
 const Navbar = () => {
-  return (
-    <div>
-        Navbar
-    </div>
-  )
+    return (
+        <div className='foody__navbar'>
+            <div className='foody__navbar-links'>
+                <div className='foody__navbar-links-logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className='foody__navbar-links-container'>
+                    <p><a href="#home">Home</a></p>
+                    <p><a href="#menu">Menu</a></p>
+                    <p><a href="#cart">Cart</a></p>
+                    <p><a href="#order">Order</a></p>
+                </div>
+            </div>
+            <div className='foody__navbar-sign'>
+                <p><a href="#signin">Sign In</a></p>
+                <button type="button">Sign Up</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
